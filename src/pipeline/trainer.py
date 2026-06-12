943ea73dd3fd9b9fe8575aa768542483fde9925f
PritRaj1 <prithvi.raj1@outlook.com>
2026-06-11 21:52:46 -0700

    eval

--- src/pipeline/trainer.py
+++ src/pipeline/trainer.py
@@ -9,61 +9,86 @@ from .opt import coupled_opt
 
 
 def load_config(config_path: str) -> ml_collections.ConfigDict:
-    """Loads YAML configuration into a ConfigDict."""
-    with open(config_path, "r") as f:
-        config = yaml.safe_load(f)
-    return ml_collections.ConfigDict(config)
+	"""Loads YAML configuration into a ConfigDict."""
+	with open(config_path, "r") as f:
+		config = yaml.safe_load(f)
+	return ml_collections.ConfigDict(config)
 
 
 class ebmTrainer:
-    def __init__(
-        self,
-        config_path: str,
-        rngs: nnx.Rngs,
-    ):
-        config = load_config(config_path, rngs)
-        model = (
-            thermoEBM(config, rngs)
-            if self.config.thermo.num_temps > 1
-            else mleEBM(config, rngs)
-        )
+	def __init__(
+		self,
+		config_path: str,
+		rngs: nnx.Rngs,
+	):
+		config = load_config(config_path, rngs)
+		model = (
+			thermoEBM(config, rngs)
+			if self.config.thermo.num_temps > 1
+			else mleEBM(config, rngs)
+		)
 
-        opt = coupled_opt(config)
-        self.st = nnx.ModelAndOptimizer(model, opt)
-        nnx.display(self.st.model)
+		opt = coupled_opt(config)
+		self.st = nnx.ModelAndOptimizer(model, opt)
+		nnx.display(self.st.model)
 
-        self.writer = metric_writers.create_default_writer(logdir=config.logging.logdir)
+		self.writer = metric_writers.create_default_writer(logdir=config.logging.logdir)
 
-        self.progress = periodic_actions.ReportProgress(
-            num_train_steps=model.num_updates
-        )
+		self.progress = periodic_actions.ReportProgress(
+			num_train_steps=model.num_updates
+		)
 
-        epoch_updates = config.training.numdata // config.training.batch_size
-        ckpt_every = config.logging.ckpt_every * epoch_updates
+		epoch_updates = config.training.numdata // config.training.batch_size
+		ckpt_every = config.logging.ckpt_every * epoch_updates
 
-        self.ckpt_manager = ocp.CheckpointManager(
-            config.logging.ckpt_dir,
-            options=ocp.CheckpointManagerOptions(
-                max_to_keep=5,
-                save_interval_steps=ckpt_every,
-                create=True,
-            ),
-        )
+		self.ckpt_manager = ocp.CheckpointManager(
+			config.logging.ckpt_dir,
+			options=ocp.CheckpointManagerOptions(
+				max_to_keep=5,
+				save_interval_steps=ckpt_every,
+				create=True,
+			),
+		)
+		self.train_loader, self.test_loader = get_loaders(config.dataset)
+		self.eval_every = config.logging.eval_every
 
-    @nnx.jit
-    def train_step(key: jax.Array, x: jax.Array) -> jax.Array:
-        key, subkey = jax.random.split(key)
-        z_prior = self.st.model.sample_prior(key, x.size(0))
-        z_posterior = self.st.model.sample_posterior(subkey, x)
+	@nnx.jit
+	def update(key: jax.Array, x: jax.Array) -> jax.Array:
+		key, subkey = jax.random.split(key)
+		z_prior = self.st.model.sample_prior(key, x.size(0))
+		z_posterior = self.st.model.sample_posterior(subkey, x)
 
-        graph, ps, st = nnx.split(self.st.model, nnx.Param, ...)
-        
-        def loss(ps_mut):
-            model = nnx.merge(graph, ps_mut, st)
-            cd = model.ebm.loss(z_posterior, z_prior)
-            recon = model.loss(x, z_posterior, z_prior)
-            return cd + recon
+		graph, ps, st = nnx.split(self.st.model, nnx.Param, ...)
 
-        loss_val, grads = nnx.value_and_grad(loss)(ps)
-        self.st.update(grads)
-        return loss_val
+		def loss(ps_mut):
+			model = nnx.merge(graph, ps_mut, st)
+			cd = model.ebm.loss(z_posterior, z_prior)
+			recon = model.loss(x, z_posterior, z_prior)
+			return cd + recon
+
+		loss_val, grads = nnx.value_and_grad(loss)(ps)
+		self.st.update(grads)
+		self.st.model.train_idx += 1
+		return loss_val, subkey
+
+	@nnx.jit
+	def eval_step(key: jax.Array, x: jax.Array) -> jax.Array:
+		key, subkey = jax.random.split(key)
+		z_prior = self.st.model.sample_prior(subkey, x.size(0))
+		return self.st.model.gen.loss(x, z_prior), subkey
+
+	def train(key: jax.Array):
+		for batch in self.train_loader:
+			loss, key = self.step(key, batch["x"])
+			self.write.write_scalars(self.st.model.train_idx, {"train_loss": loss})
+
+		if self.st.model.train_idx % eval_every == 0:
+			loss = 0.0
+			for batch in self.test_loader:
+				loss_val, key = self.eval_step(key, batch["x"])
+				loss += loss_val
+
+			loss /= len(self.test_loader)
+			self.writer.write_scalars(self.st.model.train_idx, {"test_loss": loss})
+
+		return key
