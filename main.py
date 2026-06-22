06c153fbf10bf39541d95eceaae2e76d18120c3b
PritRaj1 <prithvi.raj1@outlook.com>
2026-06-22 13:36:50 -0700

    modular configs

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 from hydra import compose, initialize
 
+MODELS = [
+	"mle_ebm",
+	"thermo_ebm",
+	"mle_kaem",
+	"thermo_kaem",
+]
 
-def load_config(name: str):
+DATASETS = [
+	"cifar10",
+	"svhn",
+	"celeba",
+]
+
+
+def load_config(model: str, dataset: str):
 	with initialize(config_path="config", version_base=None):
 		cfg = compose(
-			config_name=name,
+			config_name="base",
+			overrides=[
+				f"model={model}",
+				f"dataset={dataset}",
+			],
 		)
 	return cfg
 
 
 def main():
-	config = load_config("cifar10")
-	print(config)
+	for model in MODELS:
+		for dataset in DATASETS:
+			cfg = load_config(model, dataset)
+
+			print("=" * 80)
+			print(f"{model=} {dataset=}")
+			print(cfg)
 
 
 if __name__ == "__main__":
